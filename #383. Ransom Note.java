7a2949b5059c3ba817cd69c09ecbbb130eb3ba7f
Samira <132845068+samira-nooreen@users.noreply.github.com>
2025-09-17 20:44:27 +0530

    Create #383. Ransom Note.java

---
+++ #383. Ransom Note.java
@@ -0,0 +1,34 @@
+/*383. Ransom Note
+
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+ 
+
+Example 1:
+
+Input: ransomNote = "a", magazine = "b"
+Output: false
+Example 2:
+
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+Example 3:
+
+Input: ransomNote = "aa", magazine = "aab"
+Output: true */
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for(char ch : magazine.toCharArray())
+        count[ch - 'a']++;
+        for(char ch : ransomNote.toCharArray()){
+            count[ch - 'a']--;
+            if(count[ch - 'a'] < 0)
+            return false;
+        }
+        return true;
+    }
+}
